7237cdceb5a52d478142e8b2dddb88be07e992e5
baek-si-hyun <qortlgus100@gmail.com>
2024-06-11 17:22:17 +0900

    init

---
+++ src/main/java/com/app/nperest/NpeRestApplication.java
@@ -0,0 +1,13 @@
+package com.app.nperest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NpeRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NpeRestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/app/nperest/NpeRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.nperest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NpeRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
